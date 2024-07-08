62b485acb227ddb25f75bcf4ac5e4b46ac551e70
AndW <andrzej.wyczalkowski@accenture.com>
2024-07-08 16:04:55 +0200

    added a heroku and heroku next lines to display

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // Determine the values based on the week number
     var values = ["K.D.", "M.M.", "A.W."];
     var valuesForShift = ["A.W.", "M.M.","K,D."];
+	var valuesForHeroku = ["M.S","M.M","K.D","A.W"];
 	//for services
 	var valuesToCreateServices = ["K.D","A.W"];
     var index = (weekNumber - 25) % values.length;
@@ -21,11 +22,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		if(indexServ < 0){
 			indexServ += valuesToCreateServices.length;
 		}
+	//longer for HerokuValue
+	var indexHeroku = (weekNumber - 25) % valuesForHeroku.length;
+		if(indexHeroku < 0){
+			indexHeroku += valuesForHeroku.length;
+		}
+	var indexHerokuNext = (weekNumber - 24) % valuesForHeroku.length;
+		if (indexHerokuNext < 0) {
+		indexHerokuNext += valuesForHeroku.length;
+		}
     var currentValue = values[index];
 	var currentValueNext = values[indexNext];
     var shiftValue = valuesForShift[index];
 	var shiftValueNext = valuesForShift[indexNext];
 	var ServValue = valuesToCreateServices[indexServ];
+	var HerokuValue = valuesForHeroku[indexHeroku];
+	var HerokuValueNext = valuesForHeroku[indexHerokuNext];
 
     // Update the displayed values
     var currentValueElement = document.getElementById("currentValue");
@@ -33,11 +45,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var shiftValueElement = document.getElementById("shiftValue");
 	var shiftValueElementNext = document.getElementById("shiftValueNext");
 	var servValueElement = document.getElementById("ServValue");
+	var HerokuValueElement = document.getElementById("HerokuValue");
+	var HerokuValueNextElement = document.getElementById("HerokuValueNext");
     currentValueElement.textContent = currentValue;
 	currentValueNextElement.textContent = currentValueNext;
     shiftValueElement.textContent = shiftValue;
 	shiftValueElementNext.textContent = shiftValueNext;
 	servValueElement.textContent = ServValue;
+	HerokuValueElement.textContent = HerokuValue;
+	HerokuValueNextElement.textContent = HerokuValueNext;
 });
 
 // Function to calculate the ISO week number
